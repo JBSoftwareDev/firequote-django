5effd2f58557741a0903b83e8211a63c085ddf16
Julian Barrera <jbsoftwaredevelop@gmail.com>
2026-06-23 14:02:05 -0400

    Fix static url for Render

--- firequote/firequote/settings.py
+++ firequote/firequote/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
